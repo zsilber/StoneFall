7aebd556542b54529dd0588bfe208f4db6997cd5
Zack Silber <zs2266@columbia.edu>
2015-07-19 21:11:51 -0500

    Version 1.1

--- README.txt
+++ README.txt
@@ -1,3 +1,7 @@
-Catch the falling orbs of fire to earn experience. The orb cools down each time it bounces off the walls or floor of the glacier. The orb starts off worth +3 points, cools to +1 point, and after two bounces becomes an icy projectile that must be avoided. Use left and right arrows to move and up to jump.
+Catch the falling orbs of fire to earn experience. The orb cools down each time it bounces off the walls or floor of the glacier. The orb starts off worth +3 points, cools to +1 point, and after two bounces becomes an icy projectile that must be avoided. Use left and right arrows to move and up to jump. Shift changes the game type and allows the down arrow to be used for special abilities!
 
 All trademarks and copyrights for Pokemon are owned by Nintendo and Game Freak Inc. The creator of this game does not own the images used.
+
+Version 1: Initial upload.
+
+Version 1.1: Added game modes for two other types. Everything is functional, but I only like the fire type game play. I’m going to leave the other two types in as an easter egg. The code needs more cleaning and documentation.
--- StoneFall.js
+++ StoneFall.js
@@ -1,7 +1,8 @@
-// Cross-browser support
+// Cross-browser support for requestAnimationFrame
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
+// init
 init = function(){
   var createCanvas = newCanvas(document),
       newImage = createImage(imageData),
@@ -10,10 +11,10 @@ init = function(){
       update = updateGame(reset),
       render = renderGame(newImage, canvas);
       
-  //Add canvas
+  //add canvas to dom
   document.getElementById('canvas_container').appendChild(canvas);
   
-  //Game loop
+  //game loop
   var then = Date.now(),
   mainLoop = function(){
     var gameState = gameData,
@@ -48,48 +49,61 @@ function renderGame(createImage, canvas)
       context.drawImage(bgImage, 0, 0);
     });
         
-    if (gameData.player.isDoubleJumping)
-    {
-      playerImage = createImage('evo3j', function(){
-          context.drawImage(playerImage, player.xPos, player.yPos);
-      });
-    }
-    else if (gameData.player.isFacingRight)
-    {
-      playerImage = createImage('evo3r', function(){
-          context.drawImage(playerImage, player.xPos, player.yPos);
-      });
-    }
-    else if (!(gameData.player.isFacingRight))
-    {
-      playerImage = createImage('evo3l', function(){
-          context.drawImage(playerImage, player.xPos, player.yPos);
-      });
-    }
-    
-    if (gameData.fallingObject.secondBounce)
-    {
-      objectImage = createImage('ice', function()
-      {
-          context.drawImage(objectImage, fallingObject.xPos, fallingObject.yPos);
-      });
-    }
-    else if (gameData.fallingObject.firstBounce)
-    {
-      objectImage = createImage('fire2', function()
-      {
-          context.drawImage(objectImage, fallingObject.xPos, fallingObject.yPos);
-      });
-    }
+    var playerLink = "evo3";
+    if (gameData.player.type == 0)
+      playerLink = playerLink + "g";
+    else if (gameData.player.type == 1)
+      playerLink = playerLink + "f";
+    else if (gameData.player.type == 2)
+      playerLink = playerLink + "w";
+    if (gameData.player.isDoubleJumping || gameData.player.isDashing)
+      playerLink = playerLink + "s";
+    if (gameData.player.isFacingRight)
+      playerLink = playerLink + "r";
     else
-    {
-      objectImage = createImage('fire1', function()
+      playerLink = playerLink + "l";
+
+    if (gameData.player.isVineWhipping && gameData.player.isFacingRight)
+      vineImage = createImage("vineR", function()
       {
-          context.drawImage(objectImage, fallingObject.xPos, fallingObject.yPos);
+          context.drawImage(vineImage, player.xPos+30, player.yPos-10);
       });
-    }
+    else if (gameData.player.isVineWhipping)
+      vineImage = createImage("vineL", function()
+      {
+          context.drawImage(vineImage, player.xPos-30, player.yPos-10);
+      });    
     
-    lifeImage = createImage('evo1r', function()
+    playerImage = createImage(playerLink, function()
+    {
+      context.drawImage(playerImage, player.xPos, player.yPos);
+    });
+    
+    
+    
+    var fallLink = "fall";
+    if (gameData.fallingObject.secondBounce)
+      fallLink = fallLink + "Ice";
+    else if (gameData.fallingObject.firstBounce)
+      fallLink = fallLink + "Fire2";
+    else
+      fallLink = fallLink + "Fire1";
+    
+    objectImage = createImage(fallLink, function()
+    {
+      context.drawImage(objectImage, fallingObject.xPos, fallingObject.yPos);
+    });
+    
+    
+    var lifeLink = "evo1";
+    if (gameData.player.type == 0)
+      lifeLink = lifeLink + "gr";
+    else if (gameData.player.type == 1)
+      lifeLink = lifeLink + "fr";
+    else if (gameData.player.type == 2)
+      lifeLink = lifeLink + "wr";
+    
+    lifeImage = createImage(lifeLink, function()
     {
       for (i = 0; i < gameData.lives; i++)
       {
@@ -97,12 +111,13 @@ function renderGame(createImage, canvas)
       }
     });
     
+    
     //Display score
     context.fillStyle = "rgb(255, 149, 68)";
-	  context.font = "24px Verdana";
-	  context.textAlign = "left";
-	  context.textBaseline = "top";
-	  context.fillText("Experience: " + gameData.experience, 16, 16);
+    context.font = "24px Verdana";
+    context.textAlign = "left";
+    context.textBaseline = "top";
+    context.fillText("Experience: " + gameData.experience, 16, 16);
   };
 };
 
@@ -112,9 +127,21 @@ function updateGame(resetGame){
     var fallingObject = gameData.fallingObject;
     var width = 512;
     var height = 600;
+    
+    //Type switch
+    if (16 in player.keysDown && !player.typeChanged) //Shift
+    {
+      player.type++;
+      player.type %= 3;
+      player.typeChanged = true;
+    }
+    if (!(16 in player.keysDown))
+    {
+      player.typeChanged = false;
+    }
          
     //Handles jumping
-    if (38 in player.keysDown) //Up
+    if (38 in player.keysDown && !player.isDashing) //Up
     {
       if (!player.isDoubleJumping)
       {
@@ -122,15 +149,15 @@ function updateGame(resetGame){
         {
           player.isJumping = true;
           player.prepDoubleJump = false;
-          player.upVel = 10;
+          player.yVel = 10;
         }
-        else if (player.prepDoubleJump)
+        else if (player.prepDoubleJump && player.type == 1)
         {
           player.isDoubleJumping = true;
-          player.upVel = 10;
+          player.yVel = 10;
         }
       }
-	  }
+    }
     if (!(38 in player.keysDown))
     {
       player.prepDoubleJump = true;
@@ -138,8 +165,8 @@ function updateGame(resetGame){
     
     if (player.isJumping)
     {
-      player.yPos -= player.upVel;
-      player.upVel -= .5;
+      player.yPos -= player.yVel;
+      player.yVel -= .5;
       if (player.yPos > height-96)
       {
         player.isJumping = false;
@@ -148,27 +175,62 @@ function updateGame(resetGame){
       }
     }
 
-    //Handles possible use for down arrow
+    //Handles down arrow abilities
     if (40 in player.keysDown)
     {
-    
+      if (player.type == 0 && player.vineTimer <= 0)
+      {
+        player.isVineWhipping = true;
+        player.vineTimer = 0;
+      }
+      if (player.type == 2)
+      {
+        player.isDashing = true;
+      }  
+    }
+    if (player.isDashing)
+    {
+      if (player.xPos < width-64 && player.isFacingRight)
+        player.xPos += 2 * player.speed * modifier;
+      else if (player.xPos > 0 && !player.isFacingRight)
+        player.xPos -= 2 * player.speed * modifier;
+      player.dashDistance++;
+      if (player.dashDistance > 12)
+      {
+        player.isDashing = false;
+        player.dashDistance = 0;
+      }
     }
     
+    if (player.isVineWhipping)
+    {
+      player.vineTimer++;
+      if (player.vineTimer > 20)
+      {
+        player.isVineWhipping = false;
+        //player.vineTimer = 0;
+      }
+    }
+    else
+    {
+      player.vineTimer--;
+    }
+
     //Handles left movement
-    if (37 in player.keysDown)
+    if (37 in player.keysDown && !player.isDashing)
     {
       player.isFacingRight = false;
       if (player.xPos > 0)
-		    player.xPos -= player.speed * modifier;
-	  }
+        player.xPos -= player.speed * modifier;
+    }
     
     //Handles right movement
-    if (39 in player.keysDown)
+    if (39 in player.keysDown && !player.isDashing)
     {
       player.isFacingRight = true;
       if (player.xPos < width-64)
-		    player.xPos += player.speed * modifier;
-	  }
+        player.xPos += player.speed * modifier;
+    }
     
     //Handles falling motion
     if (fallingObject.yPos < height-64) //Falls in air
@@ -201,19 +263,43 @@ function updateGame(resetGame){
     //Reset when object settles on ground
     if (Math.abs(fallingObject.yVel) < .2 && fallingObject.yPos > height-(64+2))
       gameData = resetGame(gameData);
+    
+    //Handle vine whip
+    if (player.isVineWhipping
+        && player.xPos <= (fallingObject.xPos + 42) //Left 
+        && fallingObject.xPos <= (player.xPos + 74) //Right
+        && player.yPos <= (fallingObject.yPos + 22) //Up
+        && fallingObject.yPos <= (player.yPos + 24)) //Down
+    {
+      fallingObject.yPos -= fallingObject.yVel;
+      fallingObject.yVel = -fallingObject.yVel;
+    }
         
     //Check for and handle hit
-    if (player.xPos <= (fallingObject.xPos + 32) 
-        && fallingObject.xPos <= (player.xPos + 64) 
-        && player.yPos <= (fallingObject.yPos + 5)
-        && fallingObject.yPos <= (player.yPos + 64))
+    if (player.xPos <= (fallingObject.xPos + 32) //Left 
+        && fallingObject.xPos <= (player.xPos + 64) //Right
+        && player.yPos <= (fallingObject.yPos + 5) //Up
+        && fallingObject.yPos <= (player.yPos + 64)) //Down
     {
-      if (fallingObject.secondBounce)
+      if (player.type == 0)
         gameData.lives -= 1;
-      else if (fallingObject.firstBounce)
-        gameData.experience += 1;
-      else
-        gameData.experience += 3;
+      
+      if (player.type == 1)
+        if (fallingObject.secondBounce)
+          gameData.lives -= 1;
+        else if (fallingObject.firstBounce)
+          gameData.experience += 1;
+        else
+          gameData.experience += 3;
+      
+      if (player.type == 2)
+        if (fallingObject.secondBounce)
+          gameData.experience += 1;
+        else if (fallingObject.firstBounce)
+          gameData.lives -= 1;
+        else
+          gameData.lives -= 1;
+      
       gameData = resetGame(gameData);
     }
     return gameData;
@@ -234,6 +320,8 @@ function resetGame(canvas){
       gameData.lives = 3;
       gameData.experience = 0;
     }
+    if (gameData.player.type == 0)
+      gameData.experience++;
     return gameData;
   };
 };
@@ -266,12 +354,18 @@ var gameData =
     speed: 400, //PixPerSec
     xPos: 512 / 2,
     yPos: 600 - 96,
+    yVel: 0,
+    isFacingRight: true,
     isJumping: false,
     isDoubleJumping: false,
-    isFacingRight: true,
     prepDoubleJump: false,
-    upVel: 0,
-    keysDown: {}
+    isDashing: false,
+    dashDistance: 0,
+    isVineWhipping: false,
+    vineTimer: 0,
+    keysDown: {},
+    type: 1, //0=Grass, 1=Fire, 2=Water
+    typeChanged: false
   },
   
   fallingObject:
@@ -290,20 +384,29 @@ var gameData =
 
 var imageData =
 {
-  evo1r: "http://i.imgur.com/FAtEepv.gif",
-  evo1l: "http://i.imgur.com/MCXX0ex.gif",
-  evo2r: "http://i.imgur.com/lVVB3dx.gif",
-  evo2l: "http://i.imgur.com/YzSsYs9.gif",
-  evo3r: "http://i.imgur.com/zO9Sa9I.gif",
-  evo3l: "http://i.imgur.com/0zRrSDU.gif",
-  evo3j: "http://i.imgur.com/MHOliIv.png",
-  fire1: "http://i.imgur.com/oNAXO36.png",
-  fire2: "http://i.imgur.com/t5sekS5.png",
-  ice: "http://i.imgur.com/cExq73r.png",
-  glacierBackground: "http://i.imgur.com/c4AmR5y.jpg",
+  evo1gr: "http://i.imgur.com/6d5rQy0.gif",
+  evo3gr: "http://i.imgur.com/cwr6C9l.gif",
+  evo3gl: "http://i.imgur.com/d1kuvgj.gif",
+  vineR: "http://i.imgur.com/Pchad2n.png",
+  vineL: "http://i.imgur.com/xcLMF9O.png",
   
-  background : "http://i.imgur.com/WZfpm0W.png",
-  player: "http://pldh.net/media/pokemon/md2/sprite/006.png",
-  badGuy: "http://megaicons.net/static/img/icons_sizes/528/1941/32/gold-coin-icon.png",
-  badGuy2: "http://png-2.findicons.com/files/icons/1180/sticker_1/32/skull_bw.png"
+  evo1fr: "http://i.imgur.com/FAtEepv.gif",
+  evo1fl: "http://i.imgur.com/MCXX0ex.gif",
+  evo2fr: "http://i.imgur.com/lVVB3dx.gif",
+  evo2fl: "http://i.imgur.com/YzSsYs9.gif",
+  evo3fr: "http://i.imgur.com/zO9Sa9I.gif",
+  evo3fl: "http://i.imgur.com/0zRrSDU.gif",
+  evo3fsr: "http://i.imgur.com/MHOliIv.png",
+  evo3fsl: "http://i.imgur.com/MHOliIv.png",
+  
+  evo1wr: "http://i.imgur.com/lLJkU4y.gif",
+  evo3wr: "http://i.imgur.com/wNsRGWa.gif",
+  evo3wl: "http://i.imgur.com/9VlIZDZ.gif",
+  evo3wsr: "http://i.imgur.com/HaZnjEl.gif",
+  evo3wsl: "http://i.imgur.com/C4MJqaZ.gif",
+  
+  fallFire1: "http://i.imgur.com/oNAXO36.png",
+  fallFire2: "http://i.imgur.com/t5sekS5.png",
+  fallIce: "http://i.imgur.com/cExq73r.png",
+  glacierBackground: "http://i.imgur.com/c4AmR5y.jpg",
 };
